5b842cf605ac353b1f96fe2aeeabc7d605c5e1a3
Brian Robison <brobison@usf.edu>
2020-09-27 12:25:12 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+/* Programmer Brian Robison
+ * Tech project 3b
+ * ISM 3400
+ * The purpose of this program is to demostrae the use of arrays
+ */
+namespace Tech_porject_3b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //manually defining array 
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16, 17, 18, 19, 10, 21, 22, 23, 24, 25 };
+            //Displaying vaules of array
+            Console.WriteLine("Element Value =" + numbers[0]);
+            Console.WriteLine("Element Value =" + numbers[1]);
+            Console.WriteLine("Element Value =" + numbers[2]);
+            Console.WriteLine("Element Value =" + numbers[3]);
+            Console.WriteLine("Element Value =" + numbers[4]);
+            Console.WriteLine("Element Value =" + numbers[5]);
+            Console.WriteLine("Element Value =" + numbers[6]);
+            Console.WriteLine("Element Value =" + numbers[7]);
+            Console.WriteLine("Element Value =" + numbers[8]);
+            Console.WriteLine("Element Value =" + numbers[9]);
+            Console.WriteLine("Element Value =" + numbers[10]);
+            Console.WriteLine("Element Value =" + numbers[11]);
+            Console.WriteLine("Element Value =" + numbers[12]);
+            Console.WriteLine("Element Value =" + numbers[13]);
+            Console.WriteLine("Element Value =" + numbers[14]);
+            Console.WriteLine("Element Value =" + numbers[15]);
+            Console.WriteLine("Element Value =" + numbers[16]);
+            Console.WriteLine("Element Value =" + numbers[17]);
+            Console.WriteLine("Element Value =" + numbers[18]);
+            Console.WriteLine("Element Value =" + numbers[19]);
+            Console.WriteLine("Element Value =" + numbers[20]);
+            Console.WriteLine("Element Value =" + numbers[21]);
+            Console.WriteLine("Element Value =" + numbers[22]);
+            Console.WriteLine("Element Value =" + numbers[23]);
+            Console.WriteLine("Element Value =" + numbers[24]);
+        }//end of main
+    }//end of class
+}//end of namespace
